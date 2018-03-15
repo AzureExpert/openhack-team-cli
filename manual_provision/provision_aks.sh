4bb5a2ebc87fb8fa8707cd7c853fbff0538b1eb4
Richard Guthrie <richardguthrie@Richards-MacBook-Pro.local>
2018-03-15 16:59:12 -0700

    Add aks/acr auth

--- manual_provision/provision_aks.sh
+++ manual_provision/provision_aks.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: provision_acr.sh -i <subscriptionId> -g <resourceGroupName> -c <clusterName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+usage() { echo "Usage: provision_aks.sh -i <subscriptionId> -g <resourceGroupName> -c <clusterName> -l <resourceGroupLocation>" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
